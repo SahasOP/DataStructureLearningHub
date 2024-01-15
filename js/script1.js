e4888053d21b4740035b3d9ecdc3a88c49ec8edd
SahasOP <140003995+SahasOP@users.noreply.github.com>
2024-01-15 20:10:26 +0530

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,12 @@
+// var navbar = document.getElementById("header");
+// var prevScrollpos = window.pageYOffset;
+
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     navbar.style.top = "0";
+//   } else {
+//     navbar.style.top = "-80px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
