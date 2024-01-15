f25eb7c5c6b8b7d28c54a6590cb40b56f435f9b4
SahasOP <140003995+SahasOP@users.noreply.github.com>
2024-01-15 20:46:15 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// const toggleSwitch = document.getElementById("darkModeToggle");
+
+// toggleSwitch.addEventListener("change", function() {
+//   document.body.classList.toggle("dark-mode");
+// });
+document.getElementById('darkModeToggle').addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+});
