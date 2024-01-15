930b772e411e18247861512acfad3aa8de78a891
SahasOP <140003995+SahasOP@users.noreply.github.com>
2024-01-15 20:49:00 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// const toggleSwitch = document.getElementById("darkModeToggle");
-
-// toggleSwitch.addEventListener("change", function() {
-//   document.body.classList.toggle("dark-mode");
-// });
-document.getElementById('darkModeToggle').addEventListener('click', function() {
-  document.body.classList.toggle('dark-mode');
-});
